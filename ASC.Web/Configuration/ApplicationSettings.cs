9428a2c5015f65e270df28ce08f44ace2be6a255
Tainetroi <ngocngoctaicm@gmail.com>
2025-02-25 21:56:45 +0700

    Add project files.

---
+++ ASC.Web/Configuration/ApplicationSettings.cs
@@ -0,0 +1,7 @@
+﻿namespace ASC.Solution.Configuration
+{
+    public class ApplicationSettings
+    {
+        public String ApplicationTitle { get; set; }
+    }
+}
---
+++ ASC.Web/Services/IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace ASC.Solution.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
